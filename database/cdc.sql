13f2b683e44307402c655bd36799916d1396c9a8
Fazlur Rahman <fazznew@gmail.com>
2014-12-28 17:19:56 +0700

    Database Updates
    Export new database

--- database/cdc.sql
+++ database/cdc.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2014 at 04:45 AM
+-- Generation Time: Dec 28, 2014 at 11:15 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -50,11 +50,18 @@ INSERT INTO `alumni_email` (`id`, `email`, `is_registered`) VALUES
 CREATE TABLE IF NOT EXISTS `answer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
+  `bekerja_studi` tinyint(1) NOT NULL,
+  `jenis_bekerja_studi_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `cari_aktif` tinyint(1) NOT NULL,
+  `cari_informasi_id` int(11) NOT NULL,
+  `cari_jenis_id` int(11) NOT NULL,
+  `nama_perusahaan` text NOT NULL,
   `lama_menunggu` int(11) NOT NULL,
   `lama_bekerja` double NOT NULL,
+  `perusahaan_pribadi` tinyint(1) NOT NULL,
   `gaji_id` int(11) NOT NULL,
   `kecocokan_id` int(11) NOT NULL,
-  `status_id` int(11) NOT NULL,
   `pekerjaan` text NOT NULL,
   `alamat_pekerjaan` text NOT NULL,
   `manfaat` varchar(8) NOT NULL,
@@ -63,18 +70,21 @@ CREATE TABLE IF NOT EXISTS `answer` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `status_id` (`status_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `answer`
 --
 
-INSERT INTO `answer` (`id`, `user_id`, `lama_menunggu`, `lama_bekerja`, `gaji_id`, `kecocokan_id`, `status_id`, `pekerjaan`, `alamat_pekerjaan`, `manfaat`, `masukan`, `saran`) VALUES
-(1, 1, 1, 1, 2, 1, 1, 'programmer', 'aaaaassssss', 'Ya', 'bbbbbbb', 'ccccccccccddcddcdddd'),
-(2, 2, 1, 1, 2, 1, 1, 'programmer', 'aaaaassssss', 'Ya', 'bbbbbbb', 'ccccccccccddcddcdddd'),
-(3, 3, 1, 1, 2, 1, 1, 'programmer', 'aaaaassssss', 'Ya', 'bbbbbbb', 'ccccccccccddcddcdddd'),
-(4, 4, 1, 1, 2, 1, 1, 'programmer', 'aaaaassssss', 'Ya', 'bbbbbbb', 'ccccccccccddcddcdddd'),
-(5, 5, 1, 1, 2, 1, 1, 'programmer', 'aaaaassssss', 'Ya', 'bbbbbbb', 'ccccccccccddcddcdddd');
+INSERT INTO `answer` (`id`, `user_id`, `bekerja_studi`, `jenis_bekerja_studi_id`, `status_id`, `cari_aktif`, `cari_informasi_id`, `cari_jenis_id`, `nama_perusahaan`, `lama_menunggu`, `lama_bekerja`, `perusahaan_pribadi`, `gaji_id`, `kecocokan_id`, `pekerjaan`, `alamat_pekerjaan`, `manfaat`, `masukan`, `saran`) VALUES
+(1, 1, 0, 0, 1, 0, 0, 0, '', 1, 1, 0, 4, 1, 'Programmer', 'Gading Serpong', 'Ya', 'Tidak ada', 'tidak ada'),
+(2, 3, 0, 0, 2, 0, 0, 0, '', 0, 0, 0, 0, 1, '', '', 'Ya', 'Tidak ada', 'sarannya'),
+(3, 4, 0, 0, 1, 0, 0, 0, '', 2, 1, 0, 2, 3, 'Pekerjaan', 'Alamat Pekerjaan', 'Ya', 'Tidak ada', 'Tidak ada'),
+(4, 5, 0, 0, 1, 0, 0, 0, '', 4, 2, 0, 5, 1, 'Pekerjaan Saat Ini', 'Alamat', 'Ya', 'Tambahannya', 'Sarannya'),
+(5, 6, 0, 0, 1, 0, 0, 0, '', 5, 1, 0, 1, 3, 'Pekerjaan Saya', 'Alamat pekerjaan saya', 'Ya', 'Tambahannya', 'Sarannya'),
+(6, 7, 0, 0, 1, 0, 0, 0, '', 1, 2, 0, 3, 1, 'Pekerjaan', 'Alamatnya', 'Ya', 'Tambahannya', 'Sarannya'),
+(7, 8, 0, 0, 1, 0, 0, 0, '', 2, 1, 0, 4, 1, 'Pekerjaan', 'Alamat', 'Ya', 'Tambahan', 'Sara'),
+(8, 2, 0, 0, 1, 0, 0, 0, '', 1, 1, 0, 4, 1, 'Programmer', 'Serpong', 'Ya', 'Tambahan', 'Saran');
 
 -- --------------------------------------------------------
 
@@ -156,25 +166,16 @@ CREATE TABLE IF NOT EXISTS `post` (
   `post_category_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `post`
 --
 
 INSERT INTO `post` (`id`, `user_id`, `content`, `image`, `post_category_id`) VALUES
-(1, 1, 'Ada job nih', '', 2),
-(2, 1, 'asdfsdf', '', 1),
-(3, 1, 'sdfdsf', '', 2),
-(4, 1, 'asdfsdf', '', 2),
-(5, 1, 'asd', '', 1),
-(6, 1, 'asda', '', 0),
-(7, 1, 'Dibutuhkan Programmer', '', 1),
-(8, 1, 'Yeah Programmer', '', 2),
-(9, 1, 'Microsoft Punya nih', '', 2),
-(10, 1, 'ada lowongan nih doy', '', 1),
-(11, 1, 'asdfsdf', '', 1),
-(12, 1, 'Testing', '', 1);
+(1, 1, 'a', '', 1),
+(2, 2, 'Aa', '', 1),
+(3, 5, 'Ada job fair nih', '', 2);
 
 -- --------------------------------------------------------
 
@@ -205,6 +206,7 @@ INSERT INTO `post_category` (`id`, `name`) VALUES
 CREATE TABLE IF NOT EXISTS `program` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
+  `code` varchar(5) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
@@ -212,19 +214,19 @@ CREATE TABLE IF NOT EXISTS `program` (
 -- Dumping data for table `program`
 --
 
-INSERT INTO `program` (`id`, `name`) VALUES
-(1, 'Teknik Informatika'),
-(2, 'Teknik Sipil'),
-(3, 'Teknik Kimia'),
-(4, 'Teknik Arsitektur'),
-(5, 'Perancangan Wilayah Kota'),
-(6, 'Teknik Elektro'),
-(7, 'Teknik Mesin'),
-(8, 'Teknik Industri Pertanian'),
-(9, 'Teknik Industri'),
-(10, 'Mekatronika'),
-(11, 'Otomotif'),
-(12, 'Manajemen');
+INSERT INTO `program` (`id`, `name`, `code`) VALUES
+(1, 'Informatika', 'IF'),
+(2, 'Teknik Sipil', 'TS'),
+(3, 'Teknik Kimia', 'TK'),
+(4, 'Teknik Arsitektur', 'TA'),
+(5, 'Perancangan Wilayah Kota', 'PWK'),
+(6, 'Teknik Elektro', 'TE'),
+(7, 'Teknik Mesin', 'TM'),
+(8, 'Teknik Industri Pertanian', 'TIP'),
+(9, 'Teknik Industri', 'TI'),
+(10, 'Mekatronika', 'MT'),
+(11, 'Otomotif', 'OT'),
+(12, 'Manajemen', 'MJ');
 
 -- --------------------------------------------------------
 
@@ -246,18 +248,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `address` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `program_id` (`program_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `dob`, `type`, `program_id`, `class_of`, `phone`, `address`) VALUES
-(1, 'Doddy', 'Agung Faiskara', 'doddyaf@gmail.com', '699a8bda35b1e87d852cfc3fa211b9c5', '12-12-1992', 'admin', 1, 2010, '0833333', 'Pondok Benda'),
-(2, 'Fazlur', 'Rahman', 'f.rahman.id@gmail.com', 'e91229bfe8420a803d7db002a2dc1cb7', '24-12-1992', 'user', 1, 2010, '0833322222', 'Golden Vienna'),
-(3, 'Muhammad', 'Taufik', 'taufik@gmail.com', '76868b011b66684d4a91d4ef7e1a2651', '01-01-1992', 'user', 2, 2010, '0833322222', 'Alamatnya'),
-(4, 'Alumni', '2', 'alumni2@gmail.com', '76868b011b66684d4a91d4ef7e1a2651', '01-01-1992', 'user', 2, 2009, '0833322222', 'Alamatnya'),
-(5, 'Info 09', '1', 'alumni3@gmail.com', '76868b011b66684d4a91d4ef7e1a2651', '01-01-1992', 'user', 1, 2009, '0833322222', 'Alamatnya');
+(1, 'Doddy', 'Agung Faiskara', 'doddyaf@gmail.com', '699a8bda35b1e87d852cfc3fa211b9c5', '1992-12-12', 'admin', 1, 2010, '0833333', 'Pondok Benda'),
+(2, 'Fazlur', 'Rahman', 'fazznew@gmail.com', '202cb962ac59075b964b07152d234b70', '1992-12-24', 'user', 1, 2010, '+6282128282755', 'Golden Viena 1, Blok B6/1\r\nBSD City'),
+(3, 'Rudi Kurnia', 'Putra', 'rudi@gmail.com', 'bfcd3eee9746714ca4fcba684344bbc0', '1992-01-01', 'user', 1, 2010, '+628888888', 'Jalan'),
+(4, 'Alumni', 'IF 08', 'alumni-if-08@gmail.com', '202cb962ac59075b964b07152d234b70', '1991-01-01', 'user', 1, 2008, '+62111111', 'Alamat Alumni IF 08'),
+(5, 'Alumni', 'TE 05', 'alumni-te-05@gmail.com', '202cb962ac59075b964b07152d234b70', '1990-01-01', 'user', 6, 2005, '+628121212121', 'Jl. Kosan No. 69'),
+(6, 'Alumni', 'TM 06', 'alumni-tm-06@gmail.com', '202cb962ac59075b964b07152d234b70', '1990-12-01', 'user', 7, 2006, '+628121212121', 'Jl. Kosan No. 69'),
+(7, 'Alumni', 'TS 09', 'alumni-ts-09@gmail.com', '202cb962ac59075b964b07152d234b70', '1990-01-01', 'user', 2, 2009, '+628121212121', 'Jl. Kosan No. 69'),
+(8, 'Zickri', 'Maulida', 'zickri@gmail.com', '202cb962ac59075b964b07152d234b70', '1993-12-12', 'user', 1, 2010, '+628121212121', 'Jl. Kosan No. 69');
 
 --
 -- Constraints for dumped tables
